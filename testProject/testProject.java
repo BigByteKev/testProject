d669b15bfca17e4081aac4983442909b41607806
Kevin <siebkevi@gmail.com>
2022-04-05 23:33:09 +0200

    neuer print

--- testProject/testProject.java
+++ testProject/testProject.java
@@ -3,6 +3,8 @@ public class testProject{
     public static void main(String []args){
         int i = 1;
         System.out.println(i);
+        int a = 2;
+        System.out.println(i * a * 2);
 
     }
 }
